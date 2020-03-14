304237e80f3677dd5963f24532e5c8b8b01fa8f9
MitziYolotzin <mitziulter@gmail.com>
2020-03-13 23:03:56 -0600

    test schema

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 //const express = require('express');
 //// "start": "babel-node src/index.js "
 import express from "express";
-//middleware, enter a route with tool processed by graphql
+//middleware, enter a route with tool processed by graphql, integrate graphql in express
 import graphqlHTTP from "express-graphql";
 
 
@@ -11,14 +11,20 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.json({
-        message:'hello'
+        message: 'hello'
     })
 });
 
+//object, define allow consult
+const schema = {};
+
 //visit route in the browser, process route with module
 app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
 
 }));
+
 
 app.listen(3000, () => console.log('Server on port 3000'));
 
