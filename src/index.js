54b51846a7682359440b7d6e188311427cdf60a8
MitziYolotzin <mitziulter@gmail.com>
2020-03-13 22:54:39 -0600

    test message app get

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-//requerir modulo express
-//para crear un servidor
+//require modulo express
+//create server
 //const express = require('express');
 //// "start": "babel-node src/index.js "
 import express from "express";
+//middleware, enter a route with tool processed by graphql
 import graphqlHTTP from "express-graphql";
 
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message:'hello'
+    })
+});
+
+//visit route in the browser, process route with module
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(3000, () => console.log('Server on port 3000'));
 
